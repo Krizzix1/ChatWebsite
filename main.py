528580e576cb5de862dbc223ec59147febbfbfc5
Cameron Vella <vellacameron11@gmail.com>
2024-12-15 19:16:03 +1100

    Added button to HTML

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import database
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        userInput = request.form["name"]
+        print(userInput)
+        database.add_entry(userInput)
+        return render_template("home.html")
     return render_template("home.html")
+
 
 
 app.run(host="localhost",port=5001)
