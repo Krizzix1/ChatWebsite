07aeeb7e933600a15f8f6d79991241b63d11bc36
Justin Hoogwaerts <jhoo9750@uni.sydney.edu.au>
2024-12-16 06:15:46 +1100

    git added login and signup functionality

--- database/Queries/Schema.sql
+++ database/Queries/Schema.sql
@@ -1,5 +1,31 @@
 -- SQLite
 CREATE TABLE test(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    words TEXT
+    userName TEXT UNIQUE,
+    password TEXT,
+    salt TEXT
+);
+
+CREATE TABLE chatRoom(
+    id INTEGER,
+    user TEXT,
+    PRIMARY KEY (id,user)
+    FOREIGN KEY (user) REFERENCES users(userName)
+
+);
+
+CREATE TABLE messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sender TEXT,
+    roomReceiver INTEGER,
+    FOREIGN KEY(roomReceiver) REFERENCES chatRoom(id)
+    FOREIGN KEY(sender) REFERENCES users(userName)
+);
+
+CREATE TABLE friends(
+    userNameA TEXT,
+    userNameB TEXT,
+    PRIMARY KEY(userNameA, userNameB),
+    FOREIGN KEY(userNameA) REFERENCES users(userName),
+    FOREIGN KEY(userNameB) REFERENCES users(userName)
 );
--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+
+import database
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        action = request.form["action"]
+        if action == "login":
+            return redirect(url_for("login"))
+        elif action == "signup":
+            return redirect(url_for("signup"))
     return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if database.existing_user(username):
+            if database.validate_user_password(username, password):
+                return redirect(url_for("main"))
+            else:
+                print("Incorrect password")
+        else:
+            print(f"user {username} does not exist")
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if database.existing_user(username):
+            print(f"user {username} exists")
+        else:
+            database.add_user(username, password)
+            return redirect(url_for("login"))
+    return render_template("signup.html")
+
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
 
 
 app.run(host="localhost",port=5001)
