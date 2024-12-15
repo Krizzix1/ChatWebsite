7146ce4bf6b95165546a54f4a424df1aec2580fb
Justin Hoogwaerts <jhoo9750@uni.sydney.edu.au>
2024-12-15 18:56:13 +1100

    added basic database IO

--- database.py
+++ database.py
@@ -1,1 +1,33 @@
-import SQLAlchemy 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///database/testBase.db", echo=False)
+
+Base = declarative_base()
+
+class Test(Base):
+    __tablename__ = "test"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    words = Column(String)
+
+    def __repr__(self):
+        return f"(ID = {self.id}, words = '{self.words}')"
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+def add_entry(text):
+    with Session() as session:
+        entry = Test(words = text)
+        session.add(entry)
+        session.commit()
+
+def query_all():
+    with Session() as session:
+        rows = session.query(Test).all()
+        for row in rows:
+            print(row)
+
