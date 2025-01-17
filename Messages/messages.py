b29a75be76e50d4fc90cc3be24f4817a40d6d5ec
Cameron Vella <vellacameron11@gmail.com>
2025-01-18 00:07:21 +1100

    made navbar

--- Messages/messages.py
+++ Messages/messages.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, flash, url_for, redirect
 
 messageBP = Blueprint("messageBP",__name__,static_folder="static",template_folder="templates")
 
 @messageBP.route("/")
 def inbox():
+    if "username" not in session:
+        flash("Must be logged in")
+        return redirect(url_for("loginBP.login"))
     return render_template("messages.html")
